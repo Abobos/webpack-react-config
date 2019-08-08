567270035f3f347fed1f2e02e2c25723cfa60cbb
Blessing Abobo <suspieabobo@yahoo.com>
2019-08-08 18:16:14 +0100

    Load Css fils

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-}
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /(\.png$|\.jpg$|\.jpeg$)/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
+};
 
 module.exports = config;
